47be1f69ce774fc52b095d25913b041fd4607396
junl <jiangjunlong@meituan.com>
2020-02-10 18:06:51 +0800

    delete some

--- alg-cpp/main.cpp
+++ alg-cpp/main.cpp
@@ -69,51 +69,9 @@ public:
         return people;
     }
 };
-class Solution2 {
-public:
-    bool canPartition(vector<int>& nums) {
-      int sum = 0;
-      for (auto &i : nums){
-          sum+=i;
-      }
-      if (sum & 0x1){
-          return false;
-      }
-      int v = sum / 2;
-      int idx = -1;
-      helper2(nums, idx, v);
-      return st;
-    }
-    void helper2(vector<int &> &nums,int idx, int sum){
-        if (sum == 0){
-            st = true;
-            return;
-        }
-    
-        for(int i=idx+1; i < nums.size();i++){
-            if(sum > nums[i]){
-                helper2(nums, i, sum - nums[i]);
-                helper2(nums, i, sum);
-            }
-        }
-    }
-    bool st;
-};
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::string s{"aabccccaab"};
     std::cout << FilterRepeatString(s) << std::endl;
-    Solution so;
-//    string s2 = "3[a2[c]]";//"3[a2[c]]"
-//    s2 = so.decodeString(s2);
-    vector<vector<int>> r;
-    r.insert(r.begin()+2, {1});
-//    r.push_back({7,0});
-//    r.push_back({4,4});
-//    r.push_back({7,1});
-//    r.push_back({5,0});
-//    r.push_back({6,1});
-//    r.push_back({5,2});
-//    so.reconstructQueue(r);
     return 0;
 }
